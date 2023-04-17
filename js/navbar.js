dc5a33a5f2069150047b92880912621d4c15a266
Cheng <calvindemeester@gmail.com>
2023-04-17 16:20:14 +0200

    Added workshop and image placeholders

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector('nav');
+const header = document.querySelector('header');
+const logo = document.querySelector('nav img');
+
+// Define a function to handle scroll event
+function handleScroll() {
+  if (window.pageYOffset > header.offsetHeight) {
+    nav.classList.add('nav-scrolled');
+    logo.setAttribute('src', './images/logo-black.png');
+    logo.style.height = '75px';
+  } else {
+    nav.classList.remove('nav-scrolled');
+    logo.setAttribute('src', './images/logo-white.png');
+    logo.style.height = '150px';
+  }
+}
+
+// Call the function on initial page load
+handleScroll();
+
+// Add a scroll event listener to call the function on scroll
+window.addEventListener('scroll', handleScroll);
--- js/parallax.js
+++ js/parallax.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-    // get the background container and its image
-    var backgroundContainer = document.querySelector(".background-container");
-    var backgroundImg = backgroundContainer.querySelector("img");
-    
-    // define a function to adjust the transform of the image based on scroll position
-    function adjustParallaxTransform() {
-      var scrollPosition = window.scrollY;
-      var parallaxAmount = scrollPosition / 3;
-      backgroundImg.style.transform = "translate3d(0, calc(-50% + " + parallaxAmount + "px), 0)";
-    }
-    
-    // add a scroll event listener to call the parallax function
-    window.addEventListener("scroll", adjustParallaxTransform);
-  });
+  // get the background container and its image
+  var backgroundContainer = document.querySelector(".background-container");
+  var backgroundImg = backgroundContainer.querySelector("img");
+  
+  // define a function to adjust the transform of the image based on scroll position
+  function adjustParallaxTransform() {
+    var scrollPosition = window.scrollY;
+    var parallaxAmount = scrollPosition / 3;
+    backgroundImg.style.transform = "translate3d(0, calc(-50% + " + parallaxAmount + "px), 0)";
+  }
+  
+  // add a scroll event listener to call the parallax function
+  window.addEventListener("scroll", adjustParallaxTransform);
+  
+  // call the function initially in case the page starts scrolled
+  adjustParallaxTransform();
+});
